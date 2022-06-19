3edba97f3fe4767c68ec22c5dc9948865504125a
Adrian <adrianstruwe@web.de>
2022-06-19 07:52:19 +0200

    Remove `Option` from file hashes (#7)

--- src/structures/version_structs.rs
+++ src/structures/version_structs.rs
@@ -53,9 +53,9 @@ pub struct VersionFile {
 #[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct Hashes {
     /// The SHA512 hash of the version file
-    pub sha512: Option<String>,
+    pub sha512: String,
     /// The SHA1 hash of the version file
-    pub sha1: Option<String>,
+    pub sha1: String,
 }
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
