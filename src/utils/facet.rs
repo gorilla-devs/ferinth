849fadf1573cd862bfc32b7c96a8918e3523007f
Alice Isabel <alice@siscode.org>
2022-10-27 11:58:53 -0300

    Add `FacetBuilder` struct

--- src/utils/facet.rs
+++ src/utils/facet.rs
@@ -17,3 +17,36 @@ impl Into<String> for Facet {
         format!("{}:{}", self.key, self.value)
     }
 }
+
+struct FacetBuilder {
+    stack: Vec<Vec<Facet>>,
+    cur: Vec<Facet>,
+}
+
+impl FacetBuilder {
+    fn new(facet: Facet) -> FacetBuilder {
+        FacetBuilder {
+            stack: vec![],
+            cur: vec![facet],
+        }
+    }
+
+    fn and(mut self, facet: Facet) -> FacetBuilder {
+        self.cur.push(facet);
+        self
+    }
+
+    fn or(mut self, facet: Facet) -> FacetBuilder {
+        self.stack.push(self.cur);
+        self.cur = vec![facet];
+        self
+    }
+
+    fn build(mut self) -> Vec<Vec<String>> {
+        self.stack.push(self.cur);
+        self.stack
+            .iter()
+            .map(|x| x.iter().map(|z| z.into()))
+            .collect()
+    }
+}
