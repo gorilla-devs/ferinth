08c23901277d3ab85950a2074bc0f0af3328bd88
Alice Isabel <alice@siscode.org>
2022-10-27 12:19:04 -0300

    Add `ModrinthFacet` struct

--- src/utils/facet.rs
+++ src/utils/facet.rs
@@ -1,3 +1,5 @@
+use crate::structures::project::ProjectType;
+
 struct Facet {
     key: String,
     value: String,
@@ -48,5 +50,46 @@ impl FacetBuilder {
             .iter()
             .map(|x| x.iter().map(|z| z.into()))
             .collect()
+    }
+}
+
+struct ModrinthFacet {}
+
+impl ModrinthFacet {
+    /// Creates a valid Modrinth [`Facet`]  
+    /// # Arguments
+    /// * `version` - The minecraft version to filter the results from
+    fn version<T: Into<String>>(version: T) -> Facet {
+        Facet {
+            key: "versions".to_string(),
+            value: version.into(),
+        }
+    }
+    /// Creates a valid Modrinth [`Facet`]  
+    /// # Arguments
+    /// * `project_type` - The [`ProjectType`] to filter the results from
+    fn project_type(project_type: ProjectType) -> Facet {
+        Facet {
+            key: "project_type".to_string(),
+            value: format!("{:?}", project_type).to_lowercase(),
+        }
+    }
+    /// Creates a valid Modrinth [`Facet`]  
+    /// # Arguments
+    /// * `license` - The license ID (e.g. mit, arr) to filter the results from
+    fn license<T: Into<String>>(license: T) -> Facet {
+        Facet {
+            key: "license".to_string(),
+            value: license.into(),
+        }
+    }
+    /// Creates a valid Modrinth [`Facet`]  
+    /// # Arguments
+    /// * `category` - The loader or category to filter the results from
+    fn category<T: Into<String>>(category: T) -> Facet {
+        Facet {
+            key: "categories".to_string(),
+            value: category.into(),
+        }
     }
 }
