600e7c814cf257a1ef1a2008eef7c04d78f551d0
Ilesh Thiada <ileshkt@gmail.com>
2023-05-07 21:53:03 +0530

    Finish up team and tag api calls

--- src/api_calls/tag.rs
+++ src/api_calls/tag.rs
@@ -2,21 +2,21 @@
 //!
 //! [documentation](https://docs.modrinth.com/api-spec/#tag/tags)
 
-use crate::{
-    request::RequestBuilderCustomSend, structures::tag::*, url_ext::UrlJoinAll, Ferinth, Result,
-    API_BASE_URL,
-};
+use super::*;
+use crate::structures::tag::*;
 
 impl Ferinth {
-    /// List the categories, their icons, and applicable project types
-    ///
-    /// ```rust
-    /// # #[tokio::main]
-    /// # async fn main() -> ferinth::Result<()> {
-    /// # let modrinth = ferinth::Ferinth::default();
-    /// let categories = modrinth.list_categories().await?;
-    /// # Ok(()) }
-    /// ```
+    /**
+    List the categories, their icons, and applicable project types
+
+    ```rust
+    # #[tokio::main]
+    # async fn main() -> ferinth::Result<()> {
+    # let modrinth = ferinth::Ferinth::default();
+    let categories = modrinth.list_categories().await?;
+    # Ok(()) }
+    ```
+    */
     pub async fn list_categories(&self) -> Result<Vec<Category>> {
         self.client
             .get(API_BASE_URL.join_all(vec!["tag", "category"]))
@@ -24,15 +24,17 @@ impl Ferinth {
             .await
     }
 
-    /// List the loaders, their icons, and supported project types
-    ///
-    /// ```rust
-    /// # #[tokio::main]
-    /// # async fn main() -> ferinth::Result<()> {
-    /// # let modrinth = ferinth::Ferinth::default();
-    /// let loaders = modrinth.list_loaders().await?;
-    /// # Ok(()) }
-    /// ```
+    /**
+    List the loaders, their icons, and supported project types
+
+    ```rust
+    # #[tokio::main]
+    # async fn main() -> ferinth::Result<()> {
+    # let modrinth = ferinth::Ferinth::default();
+    let loaders = modrinth.list_loaders().await?;
+    # Ok(()) }
+    ```
+    */
     pub async fn list_loaders(&self) -> Result<Vec<Loader>> {
         self.client
             .get(API_BASE_URL.join_all(vec!["tag", "loader"]))
@@ -40,15 +42,17 @@ impl Ferinth {
             .await
     }
 
-    /// List the game versions and information about them
-    ///
-    /// ```rust
-    /// # #[tokio::main]
-    /// # async fn main() -> ferinth::Result<()> {
-    /// # let modrinth = ferinth::Ferinth::default();
-    /// let game_versions = modrinth.list_game_versions().await?;
-    /// # Ok(()) }
-    /// ```
+    /**
+    List the game versions and information about them
+
+    ```rust
+    # #[tokio::main]
+    # async fn main() -> ferinth::Result<()> {
+    # let modrinth = ferinth::Ferinth::default();
+    let game_versions = modrinth.list_game_versions().await?;
+    # Ok(()) }
+    ```
+    */
     pub async fn list_game_versions(&self) -> Result<Vec<GameVersion>> {
         self.client
             .get(API_BASE_URL.join_all(vec!["tag", "game_version"]))
@@ -56,15 +60,17 @@ impl Ferinth {
             .await
     }
 
-    /// List licenses and information about them
-    ///
-    /// ```rust
-    /// # #[tokio::main]
-    /// # async fn main() -> ferinth::Result<()> {
-    /// # let modrinth = ferinth::Ferinth::default();
-    /// let licenses = modrinth.list_licenses().await?;
-    /// # Ok(()) }
-    /// ```
+    /**
+    List licenses and information about them
+
+    ```rust
+    # #[tokio::main]
+    # async fn main() -> ferinth::Result<()> {
+    # let modrinth = ferinth::Ferinth::default();
+    let licenses = modrinth.list_licenses().await?;
+    # Ok(()) }
+    ```
+    */
     pub async fn list_licenses(&self) -> Result<Vec<License>> {
         self.client
             .get(API_BASE_URL.join_all(vec!["tag", "license"]))
@@ -72,15 +78,17 @@ impl Ferinth {
             .await
     }
 
-    /// List donation platforms and information about them
-    ///
-    /// ```rust
-    /// # #[tokio::main]
-    /// # async fn main() -> ferinth::Result<()> {
-    /// # let modrinth = ferinth::Ferinth::default();
-    /// let donation_platforms = modrinth.list_donation_platforms().await?;
-    /// # Ok(()) }
-    /// ```
+    /**
+    List donation platforms and information about them
+
+    ```rust
+    # #[tokio::main]
+    # async fn main() -> ferinth::Result<()> {
+    # let modrinth = ferinth::Ferinth::default();
+    let donation_platforms = modrinth.list_donation_platforms().await?;
+    # Ok(()) }
+    ```
+    */
     pub async fn list_donation_platforms(&self) -> Result<Vec<DonationPlatform>> {
         self.client
             .get(API_BASE_URL.join_all(vec!["tag", "donation_platform"]))
@@ -88,15 +96,17 @@ impl Ferinth {
             .await
     }
 
-    /// List valid report types
-    ///
-    /// ```rust
-    /// # #[tokio::main]
-    /// # async fn main() -> ferinth::Result<()> {
-    /// # let modrinth = ferinth::Ferinth::default();
-    /// let report_types = modrinth.list_report_types().await?;
-    /// # Ok(()) }
-    /// ```
+    /**
+    List valid report types
+
+    ```rust
+    # #[tokio::main]
+    # async fn main() -> ferinth::Result<()> {
+    # let modrinth = ferinth::Ferinth::default();
+    let report_types = modrinth.list_report_types().await?;
+    # Ok(()) }
+    ```
+    */
     pub async fn list_report_types(&self) -> Result<Vec<String>> {
         self.client
             .get(API_BASE_URL.join_all(vec!["tag", "report_type"]))
--- src/api_calls/team.rs
+++ src/api_calls/team.rs
@@ -1,23 +1,23 @@
-use super::check_id_slug;
-use crate::{
-    request::RequestBuilderCustomSend,
-    structures::user::*,
-    url_ext::{UrlJoinAll, UrlWithQuery},
-    Ferinth, Result, API_BASE_URL,
-};
+//! API calls related to teams
+//!
+//! [documentation](https://docs.modrinth.com/api-spec/#tag/teams)
+
+use super::*;
+use crate::structures::user::*;
 
 impl Ferinth {
-    /// List the members of team with ID `team_id`
-    ///
-    /// Example:
-    /// ```rust
-    /// # #[tokio::main]
-    /// # async fn main() -> ferinth::Result<()> {
-    /// # let modrinth = ferinth::Ferinth::default();
-    /// let mod_menu_team = modrinth.list_project_team_members("mOgUt4GM").await?;
-    /// assert_eq!(mod_menu_team.len(), 4);
-    /// # Ok(()) }
-    /// ```
+    /**
+    List the members of the team of the project of `project_id`
+
+    ```rust
+    # #[tokio::main]
+    # async fn main() -> ferinth::Result<()> {
+    # let modrinth = ferinth::Ferinth::default();
+    let mod_menu_team = modrinth.list_project_team_members("modmenu").await?;
+    assert_eq!(mod_menu_team.len(), 4);
+    # Ok(()) }
+    ```
+    */
     pub async fn list_project_team_members(&self, project_id: &str) -> Result<Vec<TeamMember>> {
         check_id_slug(&[project_id])?;
         self.client
@@ -26,17 +26,18 @@ impl Ferinth {
             .await
     }
 
-    /// List the members of team with ID `team_id`
-    ///
-    /// Example:
-    /// ```rust
-    /// # #[tokio::main]
-    /// # async fn main() -> ferinth::Result<()> {
-    /// # let modrinth = ferinth::Ferinth::default();
-    /// let mod_menu_team = modrinth.list_team_members("VMz4FpgB").await?;
-    /// assert_eq!(mod_menu_team.len(), 4);
-    /// # Ok(()) }
-    /// ```
+    /**
+    List the members of the team of `team_id`
+
+    ```rust
+    # #[tokio::main]
+    # async fn main() -> ferinth::Result<()> {
+    # let modrinth = ferinth::Ferinth::default();
+    let mod_menu_team = modrinth.list_team_members("VMz4FpgB").await?;
+    assert_eq!(mod_menu_team.len(), 4);
+    # Ok(()) }
+    ```
+    */
     pub async fn list_team_members(&self, team_id: &str) -> Result<Vec<TeamMember>> {
         check_id_slug(&[team_id])?;
         self.client
@@ -45,22 +46,19 @@ impl Ferinth {
             .await
     }
 
-    /// Send an invite to the user of `user_id` to join the team of `team_id`.
-    ///
-    /// REQUIRES AUTHENTICATION!
-    ///
-    /// ```no_run
-    /// # #[tokio::main]
-    /// # async fn main() -> ferinth::Result<()> {
-    /// # let modrinth = ferinth::Ferinth::new(
-    /// #     env!("CARGO_CRATE_NAME"),
-    /// #     Some(env!("CARGO_PKG_VERSION")),
-    /// #     None,
-    /// #     Some(env!("MODRINTH_TOKEN")),
-    /// # )?;
-    /// modrinth.add_user("XXXXXXXX", "YYYYYYYY").await
-    /// # }
-    /// ```
+    /**
+    Send an invite to the user of `user_id` to join the team of `team_id`
+
+    REQUIRES AUTHENTICATION and appropriate permissions!
+
+    ```no_run
+    # #[tokio::main]
+    # async fn main() -> ferinth::Result<()> {
+    # let modrinth = ferinth::Ferinth::default();
+    modrinth.add_user("XXXXXXXX", "YYYYYYYY").await
+    # }
+    ```
+    */
     pub async fn add_user(&self, team_id: &str, user_id: &str) -> Result<()> {
         #[derive(serde::Serialize)]
         struct Body<'a> {
@@ -70,26 +68,28 @@ impl Ferinth {
         self.client
             .post(API_BASE_URL.join_all(vec!["team", team_id, "members"]))
             .json(&Body { user_id })
-            .custom_send_json()
-            .await
+            .custom_send()
+            .await?;
+        Ok(())
     }
 
-    /// List the members of teams with IDs `team_ids`.
-    ///
-    /// Example:
-    /// ```rust
-    /// # #[tokio::main]
-    /// # async fn main() -> ferinth::Result<()> {
-    /// # let modrinth = ferinth::Ferinth::default();
-    /// let teams = modrinth.list_multiple_teams_members(&[
-    ///     "4reLOAKe",
-    ///     "1HMZl6Mn",
-    ///     "BZoBsPo6",
-    ///     "peSx5UYg",
-    /// ]).await?;
-    /// assert_eq!(teams.len(), 4);
-    /// # Ok(()) }
-    /// ```
+    /**
+    List the members of the teams of `team_ids`
+
+    ```rust
+    # #[tokio::main]
+    # async fn main() -> ferinth::Result<()> {
+    # let modrinth = ferinth::Ferinth::default();
+    let teams = modrinth.list_multiple_teams_members(&[
+        "4reLOAKe",
+        "1HMZl6Mn",
+        "BZoBsPo6",
+        "peSx5UYg",
+    ]).await?;
+    assert_eq!(teams.len(), 4);
+    # Ok(()) }
+    ```
+    */
     pub async fn list_multiple_teams_members(
         &self,
         team_ids: &[&str],
@@ -105,46 +105,61 @@ impl Ferinth {
             .await
     }
 
-    /// Accept an invite to join the team of `team_id`
-    ///
-    /// REQUIRES AUTHENTICATION!
-    ///
-    /// ```no_run
-    /// # #[tokio::main]
-    /// # async fn main() -> ferinth::Result<()> {
-    /// # let modrinth = ferinth::Ferinth::new(
-    /// #     env!("CARGO_CRATE_NAME"),
-    /// #     Some(env!("CARGO_PKG_VERSION")),
-    /// #     None,
-    /// #     Some(env!("MODRINTH_TOKEN")),
-    /// # )?;
-    /// modrinth.join_team("XXXXXXXX").await
-    /// # }
-    /// ```
+    /**
+    Accept an invite to join the team of `team_id`
+
+    REQUIRES AUTHENTICATION!
+
+    ```no_run
+    # #[tokio::main]
+    # async fn main() -> ferinth::Result<()> {
+    # let modrinth = ferinth::Ferinth::default();
+    modrinth.join_team("XXXXXXXX").await
+    # }
+    ```
+    */
     pub async fn join_team(&self, team_id: &str) -> Result<()> {
         self.client
             .post(API_BASE_URL.join_all(vec!["team", team_id, "join"]))
-            .custom_send_json()
-            .await
+            .custom_send()
+            .await?;
+        Ok(())
     }
 
-    /// Transfer `team_id`'s ownership to `user_id`.
-    ///
-    /// REQUIRES AUTHENTICATION!
-    ///
-    /// Example:
-    /// ```no_run
-    /// # #[tokio::main]
-    /// # async fn main() -> ferinth::Result<()> {
-    /// # let modrinth = ferinth::Ferinth::new(
-    /// #     env!("CARGO_CRATE_NAME"),
-    /// #     Some(env!("CARGO_PKG_VERSION")),
-    /// #     None,
-    /// #     Some(env!("MODRINTH_TOKEN")),
-    /// # )?;
-    /// modrinth.transfer_ownership("XXXXXXXX", "YYYYYYYY").await
-    /// # }
-    /// ```
+    /**
+    Remove the member of `user_id` from the team of `team_id`
+
+    REQUIRES AUTHENTICATION and appropriate permissions!
+
+    ```no_run
+    # #[tokio::main]
+    # async fn main() -> ferinth::Result<()> {
+    # let modrinth = ferinth::Ferinth::default();
+    modrinth.remove_member("XXXXXXXX", "YYYYYYYY").await
+    # }
+    ```
+    */
+    pub async fn remove_member(&self, team_id: &str, user_id: &str) -> Result<()> {
+        self.client
+            .delete(API_BASE_URL.join_all(vec!["team", team_id, "members", user_id]))
+            .custom_send()
+            .await?;
+        Ok(())
+    }
+
+    /**
+    Transfer ownership of the team of `team_id` to the user of `user_id`
+
+    REQUIRES AUTHENTICATION and appropriate permissions!
+
+    ```no_run
+    # #[tokio::main]
+    # async fn main() -> ferinth::Result<()> {
+    # let modrinth = ferinth::Ferinth::default();
+    modrinth.transfer_ownership("XXXXXXXX", "YYYYYYYY").await
+    # }
+    ```
+    */
     pub async fn transfer_ownership(&self, team_id: &str, user_id: &str) -> Result<()> {
         #[derive(serde::Serialize)]
         struct Body<'a> {
@@ -154,7 +169,8 @@ impl Ferinth {
         self.client
             .post(API_BASE_URL.join_all(vec!["team", team_id, "owner"]))
             .json(&Body { user_id })
-            .custom_send_json()
-            .await
+            .custom_send()
+            .await?;
+        Ok(())
     }
 }
