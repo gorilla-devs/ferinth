fb1d6f167dbde61a53a6962d39b0ac143f276d04
菘菘 <rrt467778@gmail.com>
2023-10-27 22:28:50 +0800

    fix(search): project type facet serialization

--- src/structures/search.rs
+++ src/structures/search.rs
@@ -44,7 +44,10 @@ impl Serialize for Facet {
             Facet::ProjectType(project_type) => {
                 format!(
                     "project_type:{}",
-                    serde_json::to_string(project_type).unwrap()
+                    serde_json::to_value(project_type)
+                        .unwrap()
+                        .as_str()
+                        .unwrap()
                 )
             }
         };
