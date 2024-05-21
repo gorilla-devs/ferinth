3bbc0d5e981d93254de02400be7894154ac8e59f
Ilesh Thiada <ileshkt@gmail.com>
2024-05-21 10:29:59 +0530

    Merge `wip` (#14)
    * Small changes
    * Bump version and changelog

--- src/request.rs
+++ src/request.rs
@@ -1,36 +1,28 @@
-use crate::{Error, Result};
-use async_trait::async_trait;
+use crate::Result;
 use reqwest::{RequestBuilder, Response, StatusCode};
 use serde::de::DeserializeOwned;
 
-#[async_trait]
 pub(crate) trait RequestBuilderCustomSend {
     /// Build and send `self`, and return the response
     async fn custom_send(self) -> Result<Response>;
 
-    /// Build and send `self`, and deserialise and return the response
-    async fn custom_send_json<T>(self) -> Result<T>
-    where
-        T: DeserializeOwned;
+    /// Build and send `self`, and deserialise the response to `T` and return it
+    async fn custom_send_json<T: DeserializeOwned>(self) -> Result<T>;
 }
 
-#[async_trait]
 impl RequestBuilderCustomSend for RequestBuilder {
     async fn custom_send(self) -> Result<Response> {
         Ok(check_rate_limit(self.send().await?)?.error_for_status()?)
     }
 
-    async fn custom_send_json<T>(self) -> Result<T>
-    where
-        T: DeserializeOwned,
-    {
+    async fn custom_send_json<T: DeserializeOwned>(self) -> Result<T> {
         Ok(self.custom_send().await?.json().await?)
     }
 }
 
-pub(crate) fn check_rate_limit(response: Response) -> Result<Response> {
+fn check_rate_limit(response: Response) -> Result<Response> {
     if response.status() == StatusCode::TOO_MANY_REQUESTS {
-        Err(Error::RateLimitExceeded(
+        Err(crate::Error::RateLimitExceeded(
             response
                 .headers()
                 .get("X-Ratelimit-Reset")
